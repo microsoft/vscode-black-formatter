143bb064179ab771560039ce97372a7c63a8b5ec
Paula Camargo <paulitacv25@gmail.com>
2022-05-19 12:47:32 -0700

    Add initial localization config

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Licensed under the MIT License
 
+import './setupNls.ts';
 import * as vscode from 'vscode';
 import { LanguageClient } from 'vscode-languageclient/node';
 import { restartFormatServer } from './common/formatLS';
