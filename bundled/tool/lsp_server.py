273ec3ec38af1b58e9c3e9c47a16e64bbd026fd4
Karthik Nadig <kanadig@microsoft.com>
2023-09-07 14:24:25 -0700

    Add 'scripts' path to PATH (#309)

--- bundled/tool/lsp_server.py
+++ bundled/tool/lsp_server.py
@@ -10,6 +10,7 @@ import os
 import pathlib
 import re
 import sys
+import sysconfig
 import traceback
 from typing import Any, Dict, List, Optional, Sequence, Union
 
@@ -26,6 +27,26 @@ def update_sys_path(path_to_add: str, strategy: str) -> None:
             sys.path.append(path_to_add)
 
 
+# **********************************************************
+# Update PATH before running anything.
+# **********************************************************
+def update_environ_path() -> None:
+    """Update PATH environment variable with the 'scripts' directory.
+    Windows: .venv/Scripts
+    Linux/MacOS: .venv/bin
+    """
+    scripts = sysconfig.get_path("scripts")
+    paths_variants = ["Path", "PATH"]
+
+    for var_name in paths_variants:
+        if var_name in os.environ:
+            paths = os.environ[var_name].split(os.pathsep)
+            if scripts not in paths:
+                paths.insert(0, scripts)
+                os.environ[var_name] = os.pathsep.join(paths)
+                break
+
+
 # Ensure that we can import LSP libraries, and other bundled libraries.
 BUNDLE_DIR = pathlib.Path(__file__).parent.parent
 # Always use bundled server files.
@@ -34,6 +55,7 @@ update_sys_path(
     os.fspath(BUNDLE_DIR / "libs"),
     os.getenv("LS_IMPORT_STRATEGY", "useBundled"),
 )
+update_environ_path()
 
 # **********************************************************
 # Imports needed for the language server goes below this.
