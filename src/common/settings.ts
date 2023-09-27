6365a1c10b3973a403f61030a6b6ba2cc30a4b7e
Karthik Nadig <kanadig@microsoft.com>
2023-09-27 16:24:39 -0700

    Fix linting

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -159,21 +159,23 @@ export function checkIfConfigurationChanged(e: ConfigurationChangeEvent, namespa
     return changed.includes(true);
 }
 
-export function logDefaultFormatter():void {
+export function logDefaultFormatter(): void {
     getWorkspaceFolders().forEach((workspace) => {
         let config = getConfiguration('editor', { uri: workspace.uri, languageId: 'python' });
-        if (!config){
+        if (!config) {
             config = getConfiguration('editor', workspace.uri);
-            if (!config){
+            if (!config) {
                 traceInfo('Unable to get editor configuration');
             }
         }
         const formatter = config.get<string>('defaultFormatter', '');
         traceInfo(`Default formatter is set to ${formatter} for workspace ${workspace.uri.fsPath}`);
-        if (formatter !== EXTENSION_ID){
+        if (formatter !== EXTENSION_ID) {
             traceWarn(`Black Formatter is NOT set as the default formatter for workspace ${workspace.uri.fsPath}`);
             traceWarn('To set Black Formatter as the default formatter, add the following to your settings.json file:');
-            traceWarn(`\n"[python]": {\n    "editor.defaultFormatter": "${EXTENSION_ID}",\n    "editor.formatOnSave": true\n}`);
+            traceWarn(
+                `\n"[python]": {\n    "editor.defaultFormatter": "${EXTENSION_ID}",\n    "editor.formatOnSave": true\n}`,
+            );
         }
     });
 }
