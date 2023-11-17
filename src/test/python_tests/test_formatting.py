eacaa062e33a54ac4e8f6ef5286d3527de39e713
Karthik Nadig <kanadig@microsoft.com>
2023-11-17 12:51:24 -0800

    Add tests

--- src/test/python_tests/test_formatting.py
+++ src/test/python_tests/test_formatting.py
@@ -121,3 +121,67 @@ def test_skipping_site_packages_files():
 
     expected = None
     assert_that(actual, is_(expected))
+
+
+@pytest.mark.parametrize(
+    "sample, ranges", [("sample4", "single-range"), ("sample5", "multi-range")]
+)
+def test_range_formatting(sample: str, ranges: str):
+    """Test formatting a python file."""
+    FORMATTED_TEST_FILE_PATH = constants.TEST_DATA / sample / "sample.py"
+    UNFORMATTED_TEST_FILE_PATH = constants.TEST_DATA / sample / "sample.unformatted"
+
+    contents = UNFORMATTED_TEST_FILE_PATH.read_text(encoding="utf-8")
+    lines = contents.splitlines()
+
+    actual = []
+    with utils.python_file(contents, UNFORMATTED_TEST_FILE_PATH.parent) as pf:
+        uri = utils.as_uri(str(pf))
+
+        with session.LspSession() as ls_session:
+            ls_session.initialize()
+            ls_session.notify_did_open(
+                {
+                    "textDocument": {
+                        "uri": uri,
+                        "languageId": "python",
+                        "version": 1,
+                        "text": contents,
+                    }
+                }
+            )
+
+            if ranges == "single-range":
+                actual = ls_session.text_document_range_formatting(
+                    {
+                        "textDocument": {"uri": uri},
+                        # `options` is not used by black
+                        "options": {"tabSize": 4, "insertSpaces": True},
+                        "range": {
+                            "start": {"line": 0, "character": 0},
+                            "end": {"line": 0, "character": len(lines[0])},
+                        },
+                    }
+                )
+            else:
+                actual = ls_session.text_document_ranges_formatting(
+                    {
+                        "textDocument": {"uri": uri},
+                        # `options` is not used by black
+                        "options": {"tabSize": 4, "insertSpaces": True},
+                        "ranges": [
+                            {
+                                "start": {"line": 0, "character": 0},
+                                "end": {"line": 0, "character": len(lines[0])},
+                            },
+                            {
+                                "start": {"line": 2, "character": 0},
+                                "end": {"line": 2, "character": len(lines[2])},
+                            },
+                        ],
+                    }
+                )
+
+    expected_text = FORMATTED_TEST_FILE_PATH.read_text(encoding="utf-8")
+    actual_text = utils.apply_text_edits(contents, utils.destructure_text_edits(actual))
+    assert_that(actual_text, is_(expected_text))
