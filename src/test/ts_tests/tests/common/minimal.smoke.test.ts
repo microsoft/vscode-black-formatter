c10b730cc13dc5964fb8e915c45bba261851a927
Karthik Nadig <kanadig@microsoft.com>
2024-01-31 16:42:17 -0800

    Fix test reliability

--- src/test/ts_tests/tests/common/minimal.smoke.test.ts
+++ src/test/ts_tests/tests/common/minimal.smoke.test.ts
@@ -63,16 +63,17 @@ suite('Smoke Tests', function () {
     test('Ensure Black Formatter formats a file on save', async () => {
         await vscode.commands.executeCommand('workbench.action.closeAllEditors');
         await ensurePythonExt(true);
+        const scriptPath = path.join(TEST_PROJECT_DIR, 'myscript.py');
 
         const unformatted = await fsapi.readFile(path.join(TEST_PROJECT_DIR, 'myscript.unformatted'), {
             encoding: 'utf8',
         });
         const formatted = await fsapi.readFile(path.join(TEST_PROJECT_DIR, 'myscript.formatted'), { encoding: 'utf8' });
-        await fsapi.writeFile(path.join(TEST_PROJECT_DIR, 'myscript.py'), unformatted, { encoding: 'utf8' });
+        await fsapi.writeFile(scriptPath, unformatted, { encoding: 'utf8' });
 
         await ensureBlackExt(true);
 
-        const doc = await vscode.workspace.openTextDocument(path.join(TEST_PROJECT_DIR, 'myscript.py'));
+        const doc = await vscode.workspace.openTextDocument(scriptPath);
         await vscode.window.showTextDocument(doc);
 
         const editor = vscode.window.activeTextEditor;
@@ -89,8 +90,11 @@ suite('Smoke Tests', function () {
             disposables.push(
                 watcher,
                 watcher.onDidChange((e) => {
-                    if (e.fsPath.endsWith('myscript.py')) {
+                    const text = fsapi.readFileSync(e.fsPath, { encoding: 'utf8' });
+                    if (!text.includes(';')) {
                         resolve();
+                    } else {
+                        vscode.commands.executeCommand('workbench.action.files.save');
                     }
                 }),
             );
@@ -99,7 +103,10 @@ suite('Smoke Tests', function () {
         await vscode.commands.executeCommand('workbench.action.files.save');
         await formatDone;
 
-        const actualText = await fsapi.readFile(path.join(TEST_PROJECT_DIR, 'myscript.py'), { encoding: 'utf8' });
+        const actualText = await fsapi.readFile(scriptPath, { encoding: 'utf8' });
         assert.equal(actualText, formatted);
+
+        //cleanup
+        await fsapi.writeFile(scriptPath, '', { encoding: 'utf8' });
     });
 });
